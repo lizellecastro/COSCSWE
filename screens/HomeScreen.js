3e3f29ffe878417214091abf60a47e76e7554d01
Usha Shrestha <157434543+Usha901@users.noreply.github.com>
2024-04-30 13:59:58 -0500

    Update HomeScreen.js
    I  adjusted the background picture's width and height, and made it repeat to cover the screen whenever it is resized.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -79,6 +79,8 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   backgroundImage: {
     flex: 1,
+    width:'100%',
+    height: '100%',
     resizeMode: 'cover',
     justifyContent: 'center',
   },
