ce25dec6c90be00aee1755f1d6aab1c4fc992b00
Usha Shrestha <157434543+Usha901@users.noreply.github.com>
2024-05-01 16:11:32 -0500

    Update LoginScreen.js
    added a background image, welcome message and App's name.

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,30 +1,15 @@
-import { KeyboardAvoidingView, TouchableOpacity, StyleSheet, Text, TextInput, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { useNavigation } from '@react-navigation/core'
-import { auth, createUserWithEmailAndPassword } from '../firebase';
-import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, TouchableOpacity, StyleSheet, Text, TextInput, View, ImageBackground } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
+import { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from '../firebase';
 
+const backgroundImage = require('../assets/test.jpg');
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const navigation = useNavigation()
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
-      if (user) {
-        // Check if the logged-in user is an admin
-        if (user.email === 'lizelle@test.com') {
-          navigation.replace("AdminHome");
-        } else {
-          navigation.replace("Home");
-        }
-      }
-    });
-    return unsubscribe;
-  }, []);
-
+  const navigation = useNavigation();
 
   const handleSignUp = () => {
     createUserWithEmailAndPassword(auth, email, password)
@@ -42,90 +27,112 @@ const LoginScreen = () => {
       .catch(error => alert(error.message));
   };
 
-
   return (
-    <KeyboardAvoidingView
-      style={styles.container}
-      behavior="padding"
-    >
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Email"
-          value={email}
-          onChangeText={text => setEmail(text)}
-          style={styles.input}
-        />
-        <TextInput
-          placeholder="Password"
-          value={password}
-          onChangeText={text => setPassword(text)}
-          style={styles.input}
-          secureTextEntry
-        />
-      </View>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          onPress={handleLogin}
-          style={styles.button}
-        >
-          <Text style={styles.buttonText}>Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={handleSignUp}
-          style={[styles.button, styles.buttonOutline]}
-        >
-          <Text style={styles.buttonOutlineText}>Register</Text>
-        </TouchableOpacity>
-      </View>
-    </KeyboardAvoidingView>
-  )
-}
+    <ImageBackground source={require('../assets/test.jpg')} style={styles.backgroundImage} resizeMode="repeat">
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <View style={styles.overlay}>
+          <Text style={styles.welcome}>Welcome to</Text>
+          <Text style={styles.appName}> Active Path🔥</Text>
+          <View style={styles.inputContainer}>
+            <TextInput
+              placeholder="Email"
+              value={email}
+              onChangeText={text => setEmail(text)}
+              style={styles.input}
+            />
+            <TextInput
+              placeholder="Password"
+              value={password}
+              onChangeText={text => setPassword(text)}
+              style={styles.input}
+              secureTextEntry
+            />
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity onPress={handleLogin} style={styles.button}>
+              <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={handleSignUp} style={[styles.button, styles.buttonOutline]}>
+              <Text style={styles.buttonOutlineText}>Register</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </ImageBackground>
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    inputContainer: {
-      width: '80%'
-    },
-    input: {
-      backgroundColor: 'white',
-      paddingHorizontal: 15,
-      paddingVertical: 10,
-      borderRadius: 10,
-      marginTop: 5,
-    },
-    buttonContainer: {
-      width: '60%',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: 40,
-    },
-    button: {
-      backgroundColor: '#0782F9',
-      width: '100%',
-      padding: 15,
-      borderRadius: 10,
-      alignItems: 'center',
-    },
-    buttonOutline: {
-      backgroundColor: 'white',
-      marginTop: 5,
-      borderColor: '#0782F9',
-      borderWidth: 2,
-    },
-    buttonText: {
-      color: 'white',
-      fontWeight: '700',
-      fontSize: 16,
-    },
-    buttonOutlineText: {
-      color: '#0782F9',
-      fontWeight: '700',
-      fontSize: 16,
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.3)', // Adjust the opacity as needed
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '80%', // Adjust the width of the container
+    padding: 20, // Adjust the padding of the container
+    borderRadius: 10, // Adjust the border radius of the container
+  },
+  welcome: {
+    fontSize: 36, // Adjust the font size as needed
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 10,
+  },
+  appName: {
+    fontSize: 36, // Adjust the font size as needed
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 20,
+  },
+  inputContainer: {
+    width: '100%', // Adjust the width of the input container
+    marginBottom: 20, // Adjust the margin bottom of the input container
+  },
+  input: {
+    backgroundColor: 'rgba(255, 255, 255, 0.7)',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: '100%', // Adjust the width of the button container
+    alignItems: 'center', // Center align the buttons
+  },
+  button: {
+    backgroundColor: '#0782F9',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginBottom: 10, // Adjust the margin bottom of the button
+  },
+  buttonOutline: {
+    backgroundColor: '#fff',
+    borderColor: '#0782F9',
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  buttonOutlineText: {
+    color: '#0782F9',
+    fontWeight: '700',
+    fontSize: 16,
+  }
+});
